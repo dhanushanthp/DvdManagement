4c7347d58667f1517b359eafa9752634d590d4f3
root <root@dhanu-HP-ProBook-4430s>
2015-05-19 11:54:02 +0530

    adding domains.

---
+++ src/main/java/core/domain/Employee.java
@@ -0,0 +1,22 @@
+package core.domain;
+
+public class Employee {
+	private String name;
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
--- src/main/java/core/domain/Inventory.java
+++ src/main/java/core/domain/Inventory.java
@@ -4,4 +4,28 @@ public class Inventory {
 	private long iselNum;
 	private int shelfNum;
 	private int copyNum;
+
+	public int getCopyNum() {
+		return copyNum;
+	}
+
+	public void setCopyNum(int copyNum) {
+		this.copyNum = copyNum;
+	}
+
+	public int getShelfNum() {
+		return shelfNum;
+	}
+
+	public void setShelfNum(int shelfNum) {
+		this.shelfNum = shelfNum;
+	}
+
+	public long getIselNum() {
+		return iselNum;
+	}
+
+	public void setIselNum(long iselNum) {
+		this.iselNum = iselNum;
+	}
 }
