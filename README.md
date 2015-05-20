cc2b250ac3da3e08daa9c965dfd152fcff1aae52
root <root@dhanu-HP-ProBook-4430s>
2015-05-20 12:21:19 +0530

    createing DB and Tables;

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ sudo apt-get install mysql-server
 
 to connect with server
 mysql -u uname -h localhost -p
+
+remove database
+drop database dbname
+
+show tables
+show tables;
+
+describe table_name;
--- src/main/java/core/ingest/DvdIngestion.java
+++ src/main/java/core/ingest/DvdIngestion.java
@@ -5,7 +5,8 @@ import java.sql.*;
 import core.config.Config;
 
 public class DvdIngestion {
-	public static void main(String[] args) throws SQLException {
+
+	public static void initializeDB() throws SQLException {
 		Connection conn = null;
 		Statement stmt = null;
 		try {
@@ -24,28 +25,71 @@ public class DvdIngestion {
 		}
 
 		try {
-
-			String sql = "CREATE DATABASE STUDENTS";
+			String sql = "CREATE DATABASE " + Config.DB;
 			stmt.executeUpdate(sql);
-			System.out.println("Database created successfully...");
+			System.out.println("Database " + Config.DB + " created successfully...");
 		} catch (SQLException se) {
-			String sql = "USE STUDENTS";
-			stmt.executeUpdate(sql);
+			se.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
 		}
-		
+
 		try {
+			System.out.println("Registering Database to Create Tables...");
+			Class.forName(Config.JDBC_DRIVER);
 
-			 String sql = "CREATE TABLE REGISTRATION " +
-	                 "(id INTEGER not NULL, " +
-	                 " first VARCHAR(255), " + 
-	                 " last VARCHAR(255), " + 
-	                 " age INTEGER, " + 
-	                 " PRIMARY KEY ( id ))"; 
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(Config.DB_URL + Config.DB, Config.USER, Config.PASS);
 
-			 stmt.executeUpdate(sql);
-			 System.out.println("Table has been created...");
+			System.out.println("Creating Tables...");
+			stmt = conn.createStatement();
+
+			String sql = "CREATE TABLE DVDMETA " 
+					+ "(title VARCHAR(255) not NULL, " 
+					+ " year YEAR, " 
+					+ " actor VARCHAR(255), "
+					+ " imd_rating INTEGER, " 
+					+ " format VARCHAR(50), " 
+					+ " isle_num INTEGER, " 
+					+ " self_num INTEGER, " 
+					+ " copy_num INTEGER, "
+					+ " PRIMARY KEY ( title ))";
+
+			stmt.executeUpdate(sql);
+			System.out.println("DVDMETA table has been created...");
+
+			sql = "CREATE TABLE USERS " 
+					+ "(id INTEGER not NULL, " 
+					+ " type VARCHAR(50), " 
+					+ " PRIMARY KEY ( id ))";
+
+			stmt.executeUpdate(sql);
+			System.out.println("USERS table has been created...");
+
+			sql = "CREATE TABLE DVDMANAGEMENT " 
+					+ "(reserveid INTEGER not NULL, " 
+					+ "userid INTEGER, " 
+					+ "dvdid VARCHAR(255), "
+					+ "reserved BOOLEAN, " 
+					+ "delivered BOOLEAN, " 
+					+ "FOREIGN KEY ( userid ) REFERENCES USERS(id), "
+					+ "FOREIGN KEY ( dvdid ) REFERENCES DVDMETA(title), " 
+					+ "PRIMARY KEY ( reserveid ))";
+
+			stmt.executeUpdate(sql);
+			System.out.println("DVDMANAGEMENT table has been created...");
 		} catch (SQLException se) {
 			se.printStackTrace();
 		} catch (Exception e) {
@@ -64,5 +108,10 @@ public class DvdIngestion {
 			}
 		}
 		System.out.println("Goodbye!");
+
+	}
+
+	public static void main(String[] args) throws SQLException {
+		initializeDB();
 	}
 }
